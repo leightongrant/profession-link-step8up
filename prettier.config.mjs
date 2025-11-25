fa549411b49fefdc7997d1a13a086fdd95d13ba4
Leighton Grant <oneal.grant@outlook.com>
2025-11-25 19:32:46 +0000

    feat: initialize project with root configuration and scripts

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import("prettier").Config} */
+const config = {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: "es5",
+  printWidth: 80,
+  arrowParens: "always",
+  endOfLine: "lf",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  proseWrap: "preserve",
+};
+
+export default config;
